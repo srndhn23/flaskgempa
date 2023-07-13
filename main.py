40e47048bda6ae347ac28941fde9f8ff81202eab
srndhn23 <sarindahsn23@gmail.com>
2023-07-13 20:44:10 +0700

    port

--- main.py
+++ main.py
@@ -149,5 +149,5 @@ def coba():
 
     return render_template("coba.html")
 
-if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=5000)
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
